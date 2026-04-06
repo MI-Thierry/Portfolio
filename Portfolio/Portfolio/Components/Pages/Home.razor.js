071c13a1787d40ada772225fc606a6e7be924c03
Muhirwa Thierry <mithierry1010@gmail.com>
2026-04-06 15:38:35 +0200

    Added mobile navbar

--- Portfolio/Portfolio/Components/Pages/Home.razor.js
+++ Portfolio/Portfolio/Components/Pages/Home.razor.js
@@ -11,3 +11,24 @@ window.onscroll = (event) => {
         scrolledAppbar.classList.remove("translate-0");
     }
 }
+
+window.openMobileNavbar = () => {
+    const mobileNavbar = document.getElementById("mobile-navbar");
+    const overlay = document.getElementById("overlay");
+
+    //mobileNavbar.classList.remove("hidden");
+    mobileNavbar.classList.remove("-translate-y-60");
+    mobileNavbar.classList.add("translate-0");
+
+    overlay.classList.remove("pointer-events-none");
+    overlay.classList.add("pointer-events-auto");
+
+    overlay.addEventListener("click", () => {
+        mobileNavbar.classList.remove("translate-0");
+        mobileNavbar.classList.add("-translate-y-60");
+        //mobileNavbar.classList.add("hidden");
+
+        overlay.classList.remove("pointer-events-auto");
+        overlay.classList.add("pointer-events-none");
+    })
+}
