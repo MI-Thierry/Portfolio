933ae43321425fd554f359ead755cf3143725fe6
Muhirwa Thierry <mithierry1010@gmail.com>
2026-04-06 14:36:43 +0200

    Added sticky appbar

---
+++ Portfolio/Portfolio/Components/Pages/Home.razor.js
@@ -0,0 +1,13 @@
+﻿const scrolledAppbar = document.getElementById("scrolled-appbar");
+const appbar = document.getElementById("appbar");
+const appbarHeight = appbar.getBoundingClientRect();
+
+window.onscroll = (event) => {
+    if (window.scrollY > appbarHeight.height) {
+        scrolledAppbar.classList.remove("-translate-y-20");
+        scrolledAppbar.classList.add("translate-0");
+    } else {
+        scrolledAppbar.classList.add("-translate-y-20");
+        scrolledAppbar.classList.remove("translate-0");
+    }
+}
